f3703826a0654ea8c3c357552749edba083282b5
Alejandro <46259189+Dynosaur@users.noreply.github.com>
2019-02-25 09:17:47 -0600

    Refactoring Coordinate.java

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,37 +1,26 @@
-import java.awt.*;
-
 /**
  * @author Alejandro Doberenz
  * @version 2/23/2019
  *
- * A Coordinate represents a point on the GPDraw sketchpad.
+ * A coordinate represents a position on an X and Y graph. It contains methods to compare it to other coordinates,
+ * convert it to a String, and draw it with a Pen object.
  */
 public class Coordinate implements java.io.Serializable {
 
-    private double x; // x location of the point
-    private double y; // y location of the point
-
+    private double x, y;    // The X and Y position
+    
+    // <editor-fold defaultstate="collapsed" desc="Accessor Methods">
     public double getX() {
         return x;
     }
     public double getY() {
         return y;
     }
-
-    // Get a point that is translated a given x and y coordinates over
-    public Coordinate translate(double x, double y) {
-        return new Coordinate(this.x +x, this.y + y);
+    // </editor-fold>
+    
+    public void draw(Pen pen) {
+        draw(pen, java.awt.Color.BLACK);
     }
-
-    @Override public String toString() {
-        return "(" + x + ", " + y + ")";
-    }
-    @Override public boolean equals(Object obj) {
-        if(!(obj instanceof Coordinate)) return false;
-        Coordinate coordinate = (Coordinate) obj;
-        return Math.abs((coordinate.x - x) - 1.0D) <= 1.0D && Math.abs((coordinate.y - y) - 1.0D) <= 1.0D;
-    }
-
     public void draw(Pen pen, java.awt.Color color) {
         pen.up();
         pen.move(this);
@@ -41,10 +30,23 @@ public class Coordinate implements java.io.Serializable {
         pen.move(this);
         pen.setWidth(1);
     }
-    public void draw(Pen pen) {
-        draw(pen, Color.BLACK);
+    
+    // Get a point that is a distance away from this point
+    public Coordinate translate(double x, double y) {
+        return new Coordinate(this.x +x, this.y + y);
     }
 
+    // <editor-fold defaultstate="collapsed" desc="Object Methods">
+    @Override public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+    @Override public boolean equals(Object obj) {
+        if(!(obj instanceof Coordinate)) return false;
+        Coordinate coordinate = (Coordinate) obj;
+        return Math.abs((coordinate.x - x) - 1.0D) <= 1.0D && Math.abs((coordinate.y - y) - 1.0D) <= 1.0D;
+    }
+    // </editor-fold>
+    
     public Coordinate(double x, double y) {
         this.x = x;
         this.y = y;
