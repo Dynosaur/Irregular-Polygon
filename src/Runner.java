ec0b5eced75186c7ff60a6d268f9bcbaefeec92c
Dynosaur <heracassia@gmail.com>
2019-03-23 21:33:01 -0500

    Cleaned Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -13,18 +13,6 @@ public class Runner {
         ArrayList<Coordinate> list = new ArrayList<>(IrregularPolygon.generateIrregularPolygon(4));
         LineBuilder lb = new LineBuilder(list);
         java.awt.EventQueue.invokeLater(new Thread(() -> new GUI(lb)));
-        /*
-        Coordinate[] array = {  new Coordinate(-0.89,2.19), new Coordinate(-1.21,1.19),
-                                new Coordinate(-1.75,2.415),new Coordinate(-1.51,1.8),
-                                new Coordinate(-2.01,1.45), new Coordinate(-2.81,1.61)};
-        ArrayList<Coordinate> list = new ArrayList<>(java.util.Arrays.asList(array));
-        while(list.size() > 0) {
-            Segment x = LineBuilder.findNextBestLine(list);
-            list.remove(x.getStart());
-            list.add(0, list.remove(list.indexOf(x.getEnd())));
-            System.out.println(x.getEnd());
-        }
-        */
     }
 
 }
