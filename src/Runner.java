1c56f979e4231de37c50e4b49543230d51b49666
Dynosaur <heracassia@gmail.com>
2019-03-24 23:54:49 -0500

    Disabled program from running, only runs sorting test for now

--- src/Runner.java
+++ src/Runner.java
@@ -5,14 +5,27 @@ import geometry.Segment;
 import gui.GUI;
 import gpdraw.SketchPad;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Runner {
 
     public static void main(String[] args) {
-        ArrayList<Coordinate> list = new ArrayList<>(IrregularPolygon.generateIrregularPolygon(4));
+        /*
+        ArrayList<Coordinate> list = new ArrayList<>(IrregularPolygon.generateIrregularPolygon(16));
         LineBuilder lb = new LineBuilder(list);
         java.awt.EventQueue.invokeLater(new Thread(() -> new GUI(lb)));
+        */
+        ArrayList<Segment> lines = new ArrayList<>();
+        lines.add(Segment.random(10));
+        lines.add(Segment.random(10));
+        lines.add(Segment.random(10));
+        lines.add(Segment.random(10));
+        lines.add(Segment.random(10));
+        LineBuilder.sortByLength(lines);
+        for(Segment line : lines)
+            System.out.printf("%1.2f%n", line.getDistance());
     }
 
 }
